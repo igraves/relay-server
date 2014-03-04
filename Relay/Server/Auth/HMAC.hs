8dd24b1932dbc3fcca671cdc1adc814d07c308d4
Ian Graves <thegravian@gmail.com>
2014-03-03 22:53:08 -0600

    Shimming in more nonce stuff.

--- Relay/Server/Auth/HMAC.hs
+++ Relay/Server/Auth/HMAC.hs
@@ -9,15 +9,33 @@ import Snap.Extras.JSON (writeJSON)
 
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.ByteString as BS
+import Data.ByteString.Char8 (readInteger)
 import Data.Maybe (isJust,fromJust)
 import Data.Digest.Pure.SHA
 import Data.Binary (encode)
 import Control.Monad (liftM)
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans (lift)
+import Data.Word
 
 
 lookup_secret :: MonadSnap m => Maybe Identity -> m (Maybe Secret)
 lookup_secret x = return (x >>= \_ -> Just "secret")
 
+validate_nonce :: MonadSnap m => Nonce -> m Bool
+validate_nonce bs = do
+                      nonce <- get_nonce
+                      let mb = do
+                                 (int,_) <- readInteger bs
+                                 let nonce' = fromIntegral nonce
+                                 return (int >= nonce') -- if the nonce is less than or equal, we're good, hasn't been used yet
+                      return $ (isJust mb && fromJust mb) 
+
+get_nonce :: MonadSnap m => m Word64
+get_nonce = error "Nonce state undefined"
+
+put_nonce :: MonadSnap m => Nonce -> m ()
+put_nonce = error "Nonce put not defined"
 
 lbsappend a b = LBS.append a b 
 
